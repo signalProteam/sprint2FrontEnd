6b2e9dd0d6fc30ae820d39cbb6e2e28084526add
signalProteam <signalproteam7@gmail.com>
2024-11-06 11:12:27 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Formatacao data
+
+document.addEventListener('DOMContentLoaded', function () {
+    const eventDateInput = document.getElementById('event-date');
+
+    eventDateInput.addEventListener('input', function (e) {
+        let value = e.target.value.replace(/\D/g, '');
+
+        if (value.length >= 2) {
+            value = value.slice(0, 2) + '/' + value.slice(2);
+        }
+        if (value.length >= 5) {
+            value = value.slice(0, 5) + '/' + value.slice(5, 9);
+        }
+
+        e.target.value = value;
+    });
+});
